2ae5250d1991cfee9253f0299009f2d67dcd2e5f
Nitro2000 <rishabhgarg.20000@gmail.com>
2023-02-02 15:40:22 +0530

    Splash set up

---
+++ app/src/main/java/com/example/teamhiring/ui/mainFragments/SplashFragment.kt
@@ -0,0 +1,54 @@
+package com.example.teamhiring.ui.mainFragments
+
+import android.animation.Animator
+import android.content.Context
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import com.example.teamhiring.R
+import com.example.teamhiring.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private lateinit var binding: FragmentSplashBinding
+    private lateinit var mContext: Context
+    private lateinit var mActivity: FragmentActivity
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        mContext = requireContext()
+        mActivity = requireActivity()
+        binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.dashLottie.addAnimatorListener(object: Animator.AnimatorListener {
+            override fun onAnimationStart(p0: Animator) {
+            }
+
+            override fun onAnimationEnd(p0: Animator) {
+            }
+
+            override fun onAnimationCancel(p0: Animator) {
+            }
+
+            override fun onAnimationRepeat(p0: Animator) {
+            }
+
+        })
+
+    }
+
+
+}
