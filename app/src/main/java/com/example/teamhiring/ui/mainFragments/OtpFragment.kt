51db1b0cc0604c8d8ac792b64079065935099cdd
retinadisplay <devashish.cpj2013@gmail.com>
2023-02-06 12:02:22 +0530

    home page initial design

--- app/src/main/java/com/example/teamhiring/ui/mainFragments/OtpFragment.kt
+++ app/src/main/java/com/example/teamhiring/ui/mainFragments/OtpFragment.kt
@@ -1,29 +1,42 @@
 package com.example.teamhiring.ui.mainFragments
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.teamhiring.R
+import com.example.teamhiring.databinding.FragmentOtpBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
 class OtpFragment : Fragment() {
 
 
+    private lateinit var binding: FragmentOtpBinding
+    private lateinit var mContext: Context
+    private lateinit var mActivity: Activity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_otp, container, false)
+        mContext = requireContext()
+        mActivity = requireActivity()
+        binding = FragmentOtpBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.btnOTPVerification.setOnClickListener {
+            val directions = OtpFragmentDirections.actionOtpFragmentToHomeFragmentSeeker()
+            findNavController().navigate(directions)
+        }
     }
 }
